6537b7d76a25ead551859f292c0cc9ad16d927d0
Daha-Doom <koldevay@gmail.com>
2024-10-24 18:28:08 +0700

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ТестовоеЗаданиеСлужбаДоставки
+{
+    internal class ApplicationContext: DbContext
+    {
+        public DbSet<Orders> Orders { get; set; }
+
+        public ApplicationContext() : base("Default") { }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ТестовоеЗаданиеСлужбаДоставки
+{
+    public partial class MainForm : Form
+    {
+        ApplicationContext db;
+        List<Orders> orders;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            db = new ApplicationContext();
+            orders = db.Orders.ToList();
+        }
+
+        private void chbConsiderDate_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chbConsiderDate.Checked == true)
+            {
+                dtpAfter.Enabled = false;
+                dtpBefor.Enabled = false;
+            }
+            else
+            {
+                dtpAfter.Enabled = true;
+                dtpBefor.Enabled = true;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            dgvOrders.DataSource = orders;
+        }
+    }
+}
